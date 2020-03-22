bcb3cb0c308d6e55571135c7fd5ed3d6cc9acde8
yingjiawei <yjw999wow@163.com>
2020-03-22 21:35:35 +0800

    Initial commit

---
+++ src/main/kotlin/com/pearl/jiayu/productive/ProductiveApplication.kt
@@ -0,0 +1,11 @@
+package com.pearl.jiayu.productive
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProductiveApplication
+
+fun main(args: Array<String>) {
+    runApplication<ProductiveApplication>(*args)
+}
---
+++ src/test/kotlin/com/pearl/jiayu/productive/ProductiveApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.pearl.jiayu.productive
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ProductiveApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
